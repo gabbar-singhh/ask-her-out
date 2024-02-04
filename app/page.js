963fe0850a8935e7745b6b013b136636b4ffa99a
gabbar-singhh <himanshup1308@gmail.com>
2024-02-04 16:19:48 +0530

    final

--- app/page.js
+++ app/page.js
@@ -1,95 +1,51 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [title, setTitle] = useState("would you go out with me?");
+  const [img, setImg] = useState("/assets/begging.gif");
+  const [text, setText] = useState("YES");
+
+  const [nobuttonstyle, setNobuttonstyle] = useState({});
+
+  const yesButtonHandler = (event) => {
+    console.log(event.target.innerText);
+    if (event.target.innerText === "YES") {
+      setImg("/assets/success.gif");
+      setNobuttonstyle({ display: "none" });
+      setTitle("Yayayyay! Finally 😘");
+      setText("let's fix a date");
+    }
+
+    if (event.target.innerText === "let's fix a date") {
+      window.open("https://wa.me/+917701973093");
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <main className={styles.main}>
+        <h2>{title}</h2>
+
+        <img src={img} height={300} />
+
+        <div className={styles.button_group}>
+          <button id="btn" onClick={yesButtonHandler} className={styles.button}>
+            {text}
+          </button>
+
+          <button
+            id="no_btn"
+            style={nobuttonstyle}
+            className={`${styles.button} ${styles.btn_no}`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            NO
+          </button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+      <script src="/demo.js"></script>
+    </>
   );
 }
